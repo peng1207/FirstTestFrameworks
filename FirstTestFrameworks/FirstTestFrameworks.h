c144fb7a44e9860e273c35d6f46da0ffe49e8ee6
huangshupeng <huangshupeng@huangshupengdeMacBook-Pro.local>
2016-10-23 14:39:42 +0800

    create framework

---
+++ FirstTestFrameworks/FirstTestFrameworks.h
@@ -0,0 +1,19 @@
+//
+//  FirstTestFrameworks.h
+//  FirstTestFrameworks
+//
+//  Created by huangshupeng on 2016/10/23.
+//  Copyright © 2016年 huangshupeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirstTestFrameworks.
+FOUNDATION_EXPORT double FirstTestFrameworksVersionNumber;
+
+//! Project version string for FirstTestFrameworks.
+FOUNDATION_EXPORT const unsigned char FirstTestFrameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstTestFrameworks/PublicHeader.h>
+
+
